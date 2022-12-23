3b2141d0d76b7667820fb340f1c95ffff610616e
Eduard Iskandarov <eduard.iskandarov@yandex.ru>
2022-12-23 15:16:31 +0800

    ✨ rubx token complies to permit extension

--- contracts/RUBXToken.sol
+++ contracts/RUBXToken.sol
@@ -2,9 +2,15 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
-contract RUBXToken is ERC20 {
-  constructor(uint256 initialSupply) ERC20("Digital Russian Ruble", "RUBX") {
+contract RUBXToken is ERC20, ERC20Permit {
+  constructor(
+    uint256 initialSupply
+  )
+    ERC20("Digital Russian Ruble", "RUBX")
+    ERC20Permit("Digital Russian Ruble")
+  {
     _mint(msg.sender, initialSupply);
   }
 
